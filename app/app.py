d8517938c4b0e28ee14a5af0156e90d64c51db57
Rodrigo Cristiano <rcristianofv@hotmail.com>
2024-01-28 02:38:38 -0300

    Refactor quit_game function and update script assignment

--- app/app.py
+++ app/app.py
@@ -1,20 +1,19 @@
 import os
+import pygame
 from pygame.locals import QUIT, KEYDOWN, K_ESCAPE
 
 from game.game import Game
 from config.config import Config
 from scenes.start import StartScene
 
-def quit_game(game) -> None:
-    def quit(scene, event):
+def script(game) -> None:
+    for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-            scene.logger.info("Quit game")
-            scene.game.quit()
-    game.scene.add_action(quit)
+            game.quit()
 
 if __name__ == "__main__":
     config = Config(os.path.join(os.path.dirname(__file__), 'config/config.ini'))
     game = Game(config)
-    game.script = quit_game
+    game.script = script
     start_scene = StartScene(game)
     game.run(start_scene)
